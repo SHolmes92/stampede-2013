e0b2ebd2c9467bf8f08a935d0793235eec8acaf3
biegda@student.breckschool.org <biegda@student.breckschool.org@0e676c91-682d-c11e-e80d-a1922054a673>
2012-11-10 21:10:30 +0000

    Working pneumatics. Solenoid activated on right stick buttons 8 and 9.

--- CannonBot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ CannonBot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -8,7 +8,11 @@
 package edu.wpi.first.wpilibj.templates;
 
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Solenoid;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,6 +22,13 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
+    RobotDrive drive = new RobotDrive(1,2,3,4);
+    Joystick leftStick = new Joystick(1);
+    Joystick rightStick = new Joystick(2);
+    Compressor compressor = new Compressor (1, 2);
+    Solenoid cannon1 = new Solenoid (1);
+    Solenoid cannon2 = new Solenoid (2); 
+    
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
@@ -29,6 +40,19 @@ public class RobotTemplate extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+           drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
+           drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+           drive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
+           drive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+           compressor.start();
+    
+           while (true && isOperatorControl() && isEnabled()) // loop until change 
+        {
+           // drive.mecanumDrive_Cartesian(rightStick.getX(), rightStick.getY(), leftStick.getX(), (double) 0.0 );
+           cannon1.set(rightStick.getRawButton(8));
+           cannon2.set(rightStick.getRawButton(9));
+               
+          }
+          compressor.stop();
+       }
     }
-}
